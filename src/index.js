3bdfe901346b9c7ffb37c110581f2a2c2c265695
gabrielrangel95 <gabriel.avila.rangel@gmail.com>
2018-09-01 15:00:33 -0300

    feat: implement store, reactotron and provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import 'config/ReactotronConfig';
+import store from 'store';
 import { View, Text } from 'react-native';
 
 const App = () => (
-  <View>
-    <Text>React Native</Text>
-  </View>
+  <Provider store={store}>
+    <View>
+      <Text>React Native</Text>
+    </View>
+  </Provider>
 );
 
 export default App;
